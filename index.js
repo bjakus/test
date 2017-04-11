eeb26478840c0e78896988c87cd8721d0b3dc99a
Božidar Labaš <bozidar.labas@fiveminutes.eu>
2017-04-11 14:51:25 +0200

    gsgs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 
 app.get('/apple-app-site-association', function(req, res) {
   // Note: should use a stream here, instead of fs.readFile
-  fs.readFile('./files/' + 'apple-app-site-association', function(err, data) {
+  fs.readFile('./apple-app-site-association', function(err, data) {
     if(err) {
       res.send("Oops! Couldn't find that file.");
     } else {
